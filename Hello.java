916b4a7ba0a7558e46e948310e098c0bfe182293
LeighMarie <leighmariebraswell@31-34-208.wireless.csail.mit.edu>
2014-06-17 14:40:17 -0400

    all

--- Hello.java
+++ Hello.java
@@ -3,6 +3,7 @@ public class Hello {
     public static void main(String[] args) {
         printGreeting();
     }
+    //hi from lemur
     
     public static void printGreeting() {
         System.out.println("Hello, version control!");
